c00ee264e22822dc9b90bcbbb97330b330866343
Dmitry Duev <dmitry.duev@gmail.com>
2018-09-11 15:57:15 -0700

    wip

--- resnet50.py
+++ resnet50.py
@@ -163,19 +163,19 @@ def identity_block(X, f, filters, stage, block):
     # First component of main path
     X = Conv2D(filters=F1, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2a',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    # X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X, training=1)
     X = Activation('relu')(X)
 
     # Second component of main path (≈3 lines)
     X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    # X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X, training=1)
     X = Activation('relu')(X)
 
     # Third component of main path (≈2 lines)
     X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    # X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X, training=1)
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
     X = layers.add([X_shortcut, X])
