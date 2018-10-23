a24b47e8461ae647d1ef833ff9d3663072fbf041
Dmitry Duev <dmitry.duev@gmail.com>
2018-10-23 16:11:09 -0700

    wip

--- resnet50.py
+++ resnet50.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob
 import os
+import time
 import datetime
 from PIL import Image, ImageOps
 import json
@@ -263,19 +264,22 @@ def identity_block(X, f, filters, stage, block):
     # First component of main path
     X = Conv2D(filters=F1, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2a',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X, training=1)
+    # X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X)
     X = Activation('relu')(X)
 
     # Second component of main path (≈3 lines)
     X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X, training=1)
+    # X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X)
     X = Activation('relu')(X)
 
     # Third component of main path (≈2 lines)
     X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X, training=1)
+    # X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X)
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
     X = layers.add([X_shortcut, X])
@@ -314,24 +318,28 @@ def convolutional_block(X, f, filters, stage, block, s=2):
     # First component of main path
     X = Conv2D(F1, (1, 1), strides=(s, s), name=conv_name_base + '2a',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X, training=1)
+    # X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2a')(X)
     X = Activation('relu')(X)
 
     # Second component of main path (≈3 lines)
     X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X, training=1)
+    # X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2b')(X)
     X = Activation('relu')(X)
 
     # Third component of main path (≈2 lines)
     X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c',
                kernel_initializer=glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X, training=1)
+    # X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X, training=1)
+    X = BatchNormalization(axis=-1, name=bn_name_base + '2c')(X)
 
     ##### SHORTCUT PATH #### (≈2 lines)
     X_shortcut = Conv2D(filters=F3, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '1',
                         kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
-    X_shortcut = BatchNormalization(axis=-1, name=bn_name_base + '1')(X_shortcut, training=1)
+    # X_shortcut = BatchNormalization(axis=-1, name=bn_name_base + '1')(X_shortcut, training=1)
+    X_shortcut = BatchNormalization(axis=-1, name=bn_name_base + '1')(X_shortcut)
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
     X = layers.add([X_shortcut, X])
@@ -362,7 +370,8 @@ def ResNet50(input_shape=(144, 144, 1), n_classes: int=8):
 
     # Stage 1
     X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis=-1, name='bn_conv1')(X, training=1)
+    # X = BatchNormalization(axis=-1, name='bn_conv1')(X, training=1)
+    X = BatchNormalization(axis=-1, name='bn_conv1')(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((3, 3), strides=(2, 2))(X)
 
