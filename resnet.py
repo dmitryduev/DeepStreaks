0d33a0f2c4b5f21ec3d6139ca86e3ed19a85caa1
Dmitry Duev <dmitry.duev@gmail.com>
2018-10-26 17:13:23 -0700

    wip

--- resnet.py
+++ resnet.py
@@ -539,7 +539,7 @@ if __name__ == '__main__':
 
     batch_size = 32
 
-    model.fit(X_train, Y_train, epochs=20, batch_size=batch_size, verbose=1, callbacks=[tensorboard])
+    model.fit(X_train, Y_train, epochs=50, batch_size=batch_size, verbose=1, callbacks=[tensorboard])
     # model.fit(X_train, Y_train, epochs=2, batch_size=batch_size, validation_split=0.05,
     #           verbose=1, callbacks=[tensorboard])
 
