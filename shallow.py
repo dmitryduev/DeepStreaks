dbdb31de0f1c3f769cab4e7f9849d8d4fc5d3470
Dmitry Duev <dmitry.duev@gmail.com>
2018-11-02 23:42:00 -0700

    wip

--- shallow.py
+++ shallow.py
@@ -83,7 +83,7 @@ def shallower_vgg(input_shape=(144, 144, 1), n_classes: int=1):
     # # batch norm momentum
     # batch_norm_momentum = 0.2
 
-    model = Sequential(name='ShallowVGG')
+    model = Sequential(name='ShallowerVGG')
     # input: 144x144 images with 1 channel -> (144, 144, 1) tensors.
     # this applies 16 convolution filters of size 3x3 each.
     model.add(Conv2D(16, (3, 3), activation='relu', input_shape=input_shape))
