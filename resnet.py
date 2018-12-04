d0db9a5fe16965569e02d3b033be3c5686092571
Dmitry Duev <dmitry.duev@gmail.com>
2018-12-03 16:41:03 -0800

    wip

--- resnet.py
+++ resnet.py
@@ -297,9 +297,9 @@ if __name__ == '__main__':
     loss = 'binary_crossentropy' if binary_classification else 'categorical_crossentropy'
 
     # load data
-    project_id = '5b96af9c0354c9000b0aea36'  # real vs bogus
+    # project_id = '5b96af9c0354c9000b0aea36'  # real vs bogus
     # project_id = '5b99b2c6aec3c500103a14de'  # short vs long
-    # project_id = '5be0ae7958830a0018821794'  # keep vs ditch
+    project_id = '5be0ae7958830a0018821794'  # keep vs ditch
 
     X_train, Y_train, X_test, Y_test, classes = load_data(path='./data',
                                                           project_id=project_id,
