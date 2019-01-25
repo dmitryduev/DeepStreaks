b2d3d255edc663848b50a8b6a043c6036ad941bd
Dmitry Duev <dmitry.duev@gmail.com>
2019-01-25 13:47:06 -0800

    wip: plots for paper

--- roc.py
+++ roc.py
@@ -1,7 +1,7 @@
 import os
 # force keras/tf to use CPU:
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 import tensorflow as tf
 from keras.models import model_from_json
